6a5fc0f0f891084bb533e4a5483c8e26bf970866
Graham Reeves <graham@grahamreeves.com>
2020-04-21 19:11:26 +0100

    Added int testing, new UDP code

--- Assets/OrderedInts.cs
+++ Assets/OrderedInts.cs
@@ -6,16 +6,16 @@ public class OrderedInts : MonoBehaviour
 {
 	//	handle broken up packets
 	List<byte> PendingData = new List<byte>();
-	ushort LastPacketNumber = 0;
+	uint LastPacketNumber = 0;
 	public bool FlushOnMainThread = false;
 	[Range(1, 10000)]
 	public int MaxNumbersPerFrame = 1000;
 	public int MaxBytesPerFrame { get { return MaxNumbersPerFrame * sizeof(ushort); } }
 
-	void OnNumber(ushort NextNumber)
+	void OnNumber(uint NextNumber)
 	{
 		var Step = NextNumber - LastPacketNumber;
-		if (Step != 1 )
+		if (Step != 1 && Step != 256)	//	255 when we drop an odd numbered packet and the buffer goes out of sync
 		{
 			Debug.LogWarning("Packet out of order; " + NextNumber + " last=" + LastPacketNumber + " lost=" + (Step-1) );
 		}
@@ -28,19 +28,23 @@ public class OrderedInts : MonoBehaviour
 		byte[] Data;
 		lock (PendingData)
 		{
-			var DataLength = PendingData.Count - (PendingData.Count % 2);
+			var DataLength = PendingData.Count - (PendingData.Count % sizeof(ushort));
 			DataLength = Mathf.Min(MaxBytesPerFrame, DataLength);
-			Debug.Log("Flushing " + DataLength + "bytes");
+			//Debug.Log("Flushing " + DataLength + "bytes");
 			Data = new byte[DataLength];
 			PendingData.CopyTo(0,Data,0,Data.Length);
 			PendingData.RemoveRange(0, Data.Length);
 		}
 
-		for (var i = 0; i < Data.Length; i += 2)
+		for (var i = 0; i < Data.Length; i += sizeof(ushort))
 		{
-			var a = Data[i + 0];
-			var b = Data[i + 1];
-			var n = (a << 8) | b;
+			var a = Data[i + 1];
+			var b = Data[i + 0];
+			int n = a;
+			n = (n << 8) | b;
+			//n = (n << 8) | c;
+			//n = (n << 8) | d;
+
 			var s = (ushort)n;
 			OnNumber(s);
 		}
