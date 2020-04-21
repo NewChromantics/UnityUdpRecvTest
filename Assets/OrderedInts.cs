66da451953887f99b01bc0a14c346b5cad4c2ed7
Graham Reeves <graham@grahamreeves.com>
2020-04-21 11:56:54 +0100

    Added UDP server and thing that checks for uint16's in order

---
+++ Assets/OrderedInts.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderedInts : MonoBehaviour
+{
+	ushort LastPacketNumber = 0;
+
+	void OnNumber(ushort NextNumber)
+	{
+		var Step = NextNumber - LastPacketNumber;
+		if (Step != 1 )
+		{
+			Debug.LogWarning("Packet out of order; " + NextNumber + " last=" + LastPacketNumber + " lost=" + (Step-1) );
+		}
+		LastPacketNumber = NextNumber;
+	}
+
+	public void OnPacket(byte[] Data)
+	{
+		var Alignment = Data.Length % sizeof(ushort);
+		if ( Alignment != 0 )
+		{
+			Debug.LogWarning("Alignment of incoming data x" + Data.Length + " is misaligned...");
+		}
+
+		for (var i = 0; i < Data.Length;	i+=2)
+		{
+			var a = Data[i + 0];
+			var b = Data[i + 1];
+			var n = (a << 8) | b;
+			var s = (ushort)n;
+			OnNumber(s);
+		}
+	}
+
+}
